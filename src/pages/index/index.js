8baae449102f265cdc6eb0bbd193cece0ef2982b
CAI233 <495397430@qq.com>
2019-07-01 17:50:07 +0800

    taro学习

---
+++ src/pages/index/index.js
@@ -0,0 +1,50 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button   } from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '首页'
+  }
+  constructor () {
+    super(...arguments)
+    this.state = {
+      title: '首页',
+      list: [1, 2, 3]
+    }
+  }
+
+  componentWillMount () {}
+
+  componentDidMount () {}
+
+  componentWillUpdate (nextProps, nextState) {}
+
+  componentDidUpdate (prevProps, prevState) {}
+
+  shouldComponentUpdate (nextProps, nextState) {
+    return true
+  }
+
+  add = (e) => {
+    // dosth
+  }
+
+
+  render () {
+    return (
+      <View className='index'>
+        <View className='title'>{this.state.title}</View>
+        <View className='content'>
+          {this.state.list.map(item => {
+            return (
+              <View className='item'>{item}</View>
+            )
+          })}
+          <Button className='add' onClick={this.add}>添加</Button>
+        </View>
+      </View>
+    )
+  }
+}
